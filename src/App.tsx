43b6d6afb3e291c361763475e5f7eb209903087a
Maria Aragao <eduardaaragao18@gmail.com>
2023-05-17 18:17:19 +0100

    new examples added

--- src/App.tsx
+++ src/App.tsx
@@ -2,21 +2,30 @@ import ButtonComponent from "./components/Button/Button";
 import CardComponent from "./components/Card/Card";
 import "./App.css";
 import axios from "axios";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
+import zIndex from "@mui/material/styles/zIndex";
 
 export const App = () => {
+  const [movie, setMovie] = useState({
+    title: "",
+    overview: "",
+    poster_path: "",
+  });
+
   const getMovie = async () => {
     try {
       const response = await axios.get(
-        "https://api.themoviedb.org/3/movie/76343",
+        "https://api.themoviedb.org/3/movie/76344",
         {
           params: {
-            api_key: API_KEY,
+            api_key: "0e4a5cbfa905cc6eac86c934a7763c23",
           },
         }
       );
 
-      console.log(response.data.title);
+      console.log(response.data);
+
+      setMovie(response.data);
     } catch (error) {
       console.log(error);
     }
@@ -27,8 +36,12 @@ export const App = () => {
   }, []);
 
   return (
-    <CardComponent>
-      <ButtonComponent />
-    </CardComponent>
+    <div>
+      Movie title: <span id="movie-title">{movie.title}</span>
+      <br></br>
+    </div>
+    // <CardComponent>
+    //   <ButtonComponent />
+    // </CardComponent>
   );
 };
