f6f616495ac8bec56dec63b8d69bf8fc61ebb1a6
Maria Aragao <eduardaaragao18@gmail.com>
2023-05-17 17:40:04 +0100

    Pushing new features

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,31 @@
 import ButtonComponent from "./components/Button/Button";
 import CardComponent from "./components/Card/Card";
 import "./App.css";
+import axios from "axios";
+import { useEffect } from "react";
 
 export const App = () => {
-  /* Componentes SEM children: */
+  const getMovie = async () => {
+    try {
+      const response = await axios.get(
+        "https://api.themoviedb.org/3/movie/76343",
+        {
+          params: {
+            api_key: API_KEY,
+          },
+        }
+      );
+
+      console.log(response.data.title);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    getMovie();
+  }, []);
+
   return (
     <CardComponent>
       <ButtonComponent />
